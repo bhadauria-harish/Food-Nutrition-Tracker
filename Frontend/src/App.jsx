e089275de77c472e0907577adf024ab1af359655
Aryan Raj <aryanraj120107@gmail.com>
2025-04-12 22:44:20 +0530

    Add files via upload

---
+++ Frontend/src/App.jsx
@@ -0,0 +1,34 @@
+import React from 'react'
+import Header from './components/Header';
+import Footer from './components/Footer';
+import { useState } from 'react';
+import { NavLink, Route, Routes } from 'react-router-dom';
+import Home from './components/Home';
+import About from './components/About';
+import Contact from './components/Contact';
+import TeamInfo from './components/TeamInfo';
+import Help from './components/Help';
+
+
+
+const App = () => {
+
+
+  return (
+    <>
+    <Header />
+
+    <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/teaminfo" element={<TeamInfo />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/help" element={<Help />} />
+      </Routes>
+
+    <Footer />
+    </>
+  )
+}
+
+export default App
