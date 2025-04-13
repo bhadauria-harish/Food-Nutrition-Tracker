a773d0bb30616fa5253b5e243c79a2cbe7ed8626
Aryan Raj <aryanraj120107@gmail.com>
2025-04-13 14:28:25 +0530

    Add files via upload

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,8 +1,7 @@
 import React from 'react'
 import Header from './components/Header';
 import Footer from './components/Footer';
-import { useState } from 'react';
-import { NavLink, Route, Routes } from 'react-router-dom';
+import { Route, Routes } from 'react-router-dom';
 import Home from './components/Home';
 import About from './components/About';
 import Contact from './components/Contact';
